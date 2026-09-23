5bf6cab4d84b63f84679a2164443c4faac165d19
Crico719 <christopherdaniel0120@gmail.com>
2026-09-23 06:29:22 -0500

    Revert to original game behavior

--- code0.js
+++ code0.js
@@ -487,32 +487,6 @@ gdjs.copyArray(runtimeScene.getObjects("Player"), gdjs.Game_32SceneCode.GDPlayer
 
 {
 
-
-{
-
-let isConditionTrue_0 = false;
-isConditionTrue_0 = false;
-{isConditionTrue_0 = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Space");
-}
-if (isConditionTrue_0) {
-isConditionTrue_0 = false;
-{isConditionTrue_0 = !runtimeScene.getScene().getVariables().getFromIndex(1).getAsBoolean();
-}
-}
-if (isConditionTrue_0) {
-gdjs.copyArray(runtimeScene.getObjects("Player"), gdjs.Game_32SceneCode.GDPlayerObjects1);
-{for(var i = 0, len = gdjs.Game_32SceneCode.GDPlayerObjects1.length ;i < len;++i) {
-    gdjs.Game_32SceneCode.GDPlayerObjects1[i].activateBehavior("PlatformerObject", true);
-    gdjs.Game_32SceneCode.GDPlayerObjects1[i].hide(false);
-}
-}
-{runtimeScene.getScene().getVariables().getFromIndex(1).setBoolean(true);
-}
-}
-
-}
-
-
 {
 
 let isConditionTrue_0 = false;
@@ -560,17 +534,9 @@ isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
 if (isConditionTrue_0) {
 {gdjs.evtTools.runtimeScene.resetTimer(runtimeScene, "HitCooldown");
 }
-{gdjs.copyArray(runtimeScene.getObjects("Player"), gdjs.Game_32SceneCode.GDPlayerObjects1);
-{for(var i = 0, len = gdjs.Game_32SceneCode.GDPlayerObjects1.length ;i < len;++i) {
-    gdjs.Game_32SceneCode.GDPlayerObjects1[i].activateBehavior("PlatformerObject", false);
-    gdjs.Game_32SceneCode.GDPlayerObjects1[i].hide();
-}
-}
-}
-}
 }
 
- {
+}
 
 
 {
